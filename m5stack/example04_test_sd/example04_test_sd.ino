ec7ed808805bc4a9125ebb0c001175441ee18afe
Wataru KUNINO <git@bokunimo.net>
2020-07-31 22:23:01 +0900

    自動校正機能を追加

--- m5stack/example04_test_sd/example04_test_sd.ino
+++ m5stack/example04_test_sd/example04_test_sd.ino
@@ -20,17 +20,34 @@ void setup(){                                   // 起動時に一度だけ実�
     M5.begin();                                 // M5Stack用ライブラリの起動
     pinMode(DAC_PIN, OUTPUT);                   // GPIO26を出力に
     pinMode(ADC_PIN, ANALOG);                   // GPIO36をアナログ入力に
+    mvAnalogInCalibration(ADC_PIN,DAC_PIN);     // ADCの校正処理
 }
 
 void loop(){                                    // 繰り返し実行する関数
     int dac, adc, x, y, prev;                   // 変数dacとadc,x,yを定義
 
     M5.Lcd.fillScreen(BLACK);                   // LCDを消去
-    M5.Lcd.setCursor(2, 2, 2);
     M5.Lcd.drawRect(0, 0, 320, 232, BLUE);      // 座標0,0から320x234の箱を描画
     for(y = 0; y < 231; y += 29) M5.Lcd.drawLine(0,y,319,y, BLUE);  // 罫線Y描画
     for(x = 0; x < 319; x += 40) M5.Lcd.drawLine(x,0,x,231, BLUE);  // 罫線X描画
-
+    switch(mode){
+        case 1:
+            M5.Lcd.setCursor(2, 2, 2);  M5.Lcd.print("mode = Cor");
+            M5.Lcd.setCursor(2, 18, 2); M5.Lcd.print("gain = " + String(get_mvAnalogIn_GAIN(),3));
+            M5.Lcd.setCursor(2, 34, 2); M5.Lcd.print("offs = " + String(get_mvAnalogIn_OFFSET(),3));
+            break;
+        case 2:
+            M5.Lcd.setCursor(2, 2, 2);  M5.Lcd.print("mode = Cor+Att");
+            M5.Lcd.setCursor(2, 18, 2); M5.Lcd.print("gain = " + String(get_mvAnalogIn_GAIN(),3));
+            M5.Lcd.setCursor(2, 34, 2); M5.Lcd.print("offs = " + String(get_mvAnalogIn_OFFSET(),3));
+            break;
+        default:
+            M5.Lcd.setCursor(2, 2, 2);  M5.Lcd.print("mode = Raw");
+            M5.Lcd.setCursor(2, 18, 2); M5.Lcd.print("gain = 1.000");
+            M5.Lcd.setCursor(2, 34, 2); M5.Lcd.print("offs = 0.000");
+            break;
+    }
+    float err = 0.0;
     char csvfile[10] = "/adc?.csv";
     char bmpfile[10] = "/adc?.bmp";
     csvfile[4] = (char)mode + '0';
@@ -62,26 +79,39 @@ void loop(){                                    // 繰り返し実行する関�
             snprintf(s, 256, "%d, %d\n", 16 * dac, adc);
             file.print(s);
             prev = dac;
+            err += pow((double)(dac * 16 - adc), 2.);
         }
     }
+    err = sqrt(err);
+    M5.Lcd.setCursor(2, 50, 2); M5.Lcd.print("err2 = " + String(err,1));
+    
     if(file){
         file.close();
         bmpScreenServer(bmpfile);               // スクリーンショットを保存
     }else{
-        M5.Lcd.println("SD Card ERROR");
+        M5.Lcd.setCursor(2, 50, 2);
+        M5.Lcd.print("SD Card ERROR");
         delay(1000);
     }
     M5.Speaker.begin();                         // M5Stack用スピーカの起動
     M5.Speaker.tone(440);                       // スピーカ出力 440Hzの音を出力
     delay(100);                                 // 100msの待ち時間処理
     M5.Speaker.end();                           // スピーカ出力を停止する
-    M5.Lcd.println("Press the button [A]:Raw [B]:Cor [C]:Cor+Att");
+    M5.Lcd.setCursor(116, 204, 2);
+    M5.Lcd.print("Press a button");
+    char btn[3][3][2]={{"v"," "," "},{" ","v"," "},{" "," ","v"}};
+    M5.Lcd.setCursor(44, 220, 2);
+    M5.Lcd.print("["+ String(btn[mode][0]) +"] Raw");
+    M5.Lcd.setCursor(136, 220, 2);
+    M5.Lcd.print("["+ String(btn[mode][1]) +"] Cor");
+    M5.Lcd.setCursor(230, 220, 2);
+    M5.Lcd.print("["+ String(btn[mode][2]) +"] Cor+Att");
     mode = -1;
     while(mode < 0){
         M5.update();                            // ボタン情報を更新
         if(M5.BtnA.wasPressed()) mode = 0;      // ボタンAが押されていた時
-        if(M5.BtnB.wasPressed()) mode = 1;      // ボタンAが押されていた時
-        if(M5.BtnC.wasPressed()) mode = 2;      // ボタンAが押されていた時
+        if(M5.BtnB.wasPressed()) mode = 1;      // ボタンBが押されていた時
+        if(M5.BtnC.wasPressed()) mode = 2;      // ボタンCが押されていた時
         delay(1);
     }
 }
--- m5stack/example04_test_sd/lib_mvAnalogIn.ino
+++ m5stack/example04_test_sd/lib_mvAnalogIn.ino
@@ -10,8 +10,26 @@ float mvAnalogIn(uint8_t PIN, float offset)
 https://github.com/bokunimowakaru/esp/blob/master/2_example/example36_le/
 *******************************************************************************/
 
-#define mvAnalogIn_GAIN 1.09
-#define mvAnalogIn_OFFSET -0.02           // 動作最小電圧 0.0 ～ 0.1(V)程度
+//#define DEBUG_ADC
+#define DEBUG_ADC_CAL
+
+#ifndef FLT_MAX
+    #define FLT_MAX 3.4028235E+38
+#endif
+#ifndef INT32_MAX
+    #define INT32_MAX 4096*256
+#endif
+
+float mvAnalogIn_GAIN = 1.09;
+float mvAnalogIn_OFFSET = -0.02;
+
+float get_mvAnalogIn_GAIN(){
+	return mvAnalogIn_GAIN;
+}
+
+float get_mvAnalogIn_OFFSET(){
+	return mvAnalogIn_OFFSET;
+}
 
 float mvAnalogIn_correction(int adc, float offset, float gain){
     float ad3;
@@ -23,13 +41,14 @@ float mvAnalogIn_correction(int adc, float offset, float gain){
     }else{
         ad3 = 8.378998e-4 * (float)adc + 8.158714e-2 + offset;
     }
-    ad3 *= mvAnalogIn_GAIN;
+    ad3 *= gain;
     if( ad3 < 0.0 ) ad3 = 0.0;
-    if( ad3 > 3.3 ) ad3 = 3.3;
-    Serial.print("ADC (ATT=3;11dB) = ");
-    Serial.print(ad3,3);
-    Serial.print(" [V], ");
-    Serial.println(adc);
+    #ifdef DEBUG_ADC
+        Serial.print("ADC (ATT=3;11dB) = ");
+        Serial.print(ad3,3);
+        Serial.print(" [V], ");
+        Serial.println(adc);
+    #endif
     return ad3 * 1000.;
 }
 
@@ -44,10 +63,12 @@ float mvAnalogIn(uint8_t PIN, float offset, float gain){
         analogSetPinAttenuation(PIN,ADC_0db);
         in0=analogRead(PIN);
         ad0 = 2.442116e-4 * (float)in0 + offset;
-        Serial.print("ADC (ATT=0; 0dB) = ");
-        Serial.print(ad0,3);
-        Serial.print(" [V], "); 
-        Serial.println(in0);
+        #ifdef DEBUG_ADC
+            Serial.print("ADC (ATT=0; 0dB) = ");
+            Serial.print(ad0,3);
+            Serial.print(" [V], "); 
+            Serial.println(in0);
+        #endif
         if( in3 >= 100 ){
             ad3 = ad3 * ((float)in3 - 100.) / 100.
                 + ad0 * (200. - (float)in3) / 100.;
@@ -69,6 +90,67 @@ float mvAnalogIn(uint8_t PIN){
     return mvAnalogIn(PIN, mvAnalogIn_OFFSET, mvAnalogIn_GAIN); 
 }
 
+float mvAnalogInCalibration(uint8_t PIN, uint8_t POUT){
+    int err, err_min = INT32_MAX;
+    #ifdef m5
+        M5.Lcd.printf("mvAnalogInCalibration GPIO%d -> %d\n", PIN, POUT);
+    #endif
+    dacWrite(POUT, 0);
+    delay(100);
+    err = adc_adapted_att(PIN);
+    dacWrite(POUT, 255);
+    delay(100);
+    err += abs(adc_adapted_att(PIN) - 4095);
+    if( err > 200){
+        Serial.println("Cal. ERROR");
+        #ifdef m5
+            M5.Lcd.println("Cal. ERROR");
+            delay(3000);
+        #endif
+        return -999;
+    }
+    for(float gain = .81; gain < 1.20; gain += 0.01){
+        for(float offset = -0.10; offset < 0.10; offset += 0.005){
+            err = 0;
+            for(int dac = 0; dac < 256; dac++){
+                dacWrite(POUT, dac);
+                int adc = (int)(mvAnalogIn(PIN, offset, gain) / 3300. * 4095. + 0.5);
+                // err += pow((double)(dac * 16 - adc), 2.);
+                err += abs(dac * 16 - adc);
+                #ifdef DEBUG_ADC_CAL
+                //  if(dac % 32 == 0) Serial.printf("{'d':%d}, {'a':%d}, {'e':%d}\n",dac * 16, adc, err);
+                #endif
+            }
+            // err = sqrt(err);
+            if(err < err_min){
+                mvAnalogIn_GAIN = gain;
+                mvAnalogIn_OFFSET = offset;
+                err_min = err;
+                #ifdef DEBUG_ADC_CAL
+                    Serial.printf("Cal. gain=%.3f, offset=%.3f, err=%.1f\n",gain, offset, (float)err/256.);
+                #endif
+                #ifdef m5
+                    M5.Lcd.printf("%.0f ",(float)err/256.);
+                #endif
+            }
+        }
+    }
+    #ifdef DEBUG_ADC_CAL
+        Serial.printf("     gain  = %.3f\n", mvAnalogIn_GAIN);
+        Serial.printf("     offset= %.3f\n", mvAnalogIn_OFFSET);
+        Serial.printf("     error = %.3f\n", (float)err_min / 256.);
+    #endif
+    #ifdef m5
+        M5.Lcd.printf("\ngain = %.3f", mvAnalogIn_GAIN);
+        M5.Lcd.printf(", offset = %.3f", mvAnalogIn_OFFSET);
+        M5.Lcd.printf(", error = %.3f\n", (float)err_min / 256.);
+        delay(3000);
+    #endif
+    return err_min;
+}
+
+////////////////////////////////////////////////////////////////////////
+
 float ad_correction(int adc, float offset, float gain){
     return mvAnalogIn_correction(adc, offset, gain) / 3300. * 4095.;
 }
@@ -82,13 +164,18 @@ float ad_correction(int adc){
 }
 
 float adc_adapted_att(uint8_t PIN, float offset, float gain){
-    return mvAnalogIn(PIN,offset, gain) / 3300. * 4095.;
+    float ad = mvAnalogIn(PIN,offset, gain) / 3300. * 4095.;
+    int adc = int( ad + 0.5 );
+    if( adc < 0 ) adc = 0;
+    if( adc > 4095 ) adc = 4095;
+    return adc;
 }
 
 float adc_adapted_att(uint8_t PIN, float offset){
-    return mvAnalogIn(PIN, offset, mvAnalogIn_GAIN) / 3300. * 4095.;
+    return adc_adapted_att(PIN, offset, mvAnalogIn_GAIN);
 }
 
 float adc_adapted_att(uint8_t PIN){
-    return mvAnalogIn(PIN, mvAnalogIn_OFFSET, mvAnalogIn_GAIN) / 3300. * 4095.;
+    return adc_adapted_att(PIN, mvAnalogIn_OFFSET, mvAnalogIn_GAIN);
 }
+
